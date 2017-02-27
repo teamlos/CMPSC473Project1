49a36b61ee817e7c4a8ae8f3b441bb3687d96bee
Carlos.Echevarria <carlos742309@yahoo.com>
2017-02-27 17:50:56 -0500

    new branch

--- myscheduler.cpp
+++ myscheduler.cpp
@@ -1,10 +1,11 @@
 //myschedule.cpp
 /*Define all the functions in 'myschedule.h' here.*/
 #include "myscheduler.h"
+#include <thread>
+#include <vector>
 
 void MyScheduler::CreateThread(int arriving_time, int remaining_time, int priority, int tid) //Thread ID not Process ID
 {
-
 	Holder holde;
 	//Function to Create Thread(s) and insert them in the student
 	//defined data structure
